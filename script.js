15062d11e398d9f80fd8cf67b78ec5c7c85e24b9
wizewizzard <kingaal93@gmail.com>
2022-11-21 21:32:52 +0700

    Migrate to emitting events model

--- script.js
+++ script.js
@@ -1,55 +1,10 @@
-import Generator from "./src/main/generator/generator.js";
-import Selector from './src/main/game/selector.js'
 import getDifficulty from "./src/main/game/difficulty-range.js";
-import { EventEmitter, winCheckSubscriber, winConditionSubscriber, events } from "./src/main/game/gameLifeCycle.js";
-
-const generator = new Generator();
+import { EventEmitter, startGameSubscriber, events } from "./src/main/game/gameLifeCycle.js";
+import { getFieldUI, getSelectorUI } from "./src/main/ui/render.js";
 
 document.addEventListener('DOMContentLoaded', function(){
-
-    function renderField(field){
-        if(!field){
-            console.error("No field to render");
-            return;
-        }
-        let i = 0;
-        for(const cell of field){
-            const cellElement = document.querySelector('#sudoku-grid').querySelector(`.cell[data-index="${i}"]`)
-            const supposedValuesDiv = cellElement.querySelector('.supposedValues');
-            if(cell.getValue() != null){
-                if(!cell.isModifiable()){
-                    cellElement.classList.add("unmodifiable");
-                }
-                supposedValuesDiv.classList.add('hidden');
-                cellElement.querySelector('.value').textContent = cell.getValue();
-            }
-            else{
-                cellElement.querySelector('.value').textContent = '';
-                if(cell.getSupposedValues() && cell.getSupposedValues().length > 0){
-                    supposedValuesDiv.classList.remove('hidden');
-                    const supposedValuesString = cell.getSupposedValues().join(' ');
-                    supposedValuesDiv.textContent = supposedValuesString;
-                }
-                else{
-                    supposedValuesDiv.classList.add('hidden');
-                }
-            } 
-            i++;
-        }
-    }
-
-    function renderSelector(selector, lx, ty ){
-        if(selector.getSelectedCell()){
-            //selector.getSelectedCell()
-        }
-        if(lx && ty){
-            selectorElement.style.left = lx +"px";
-            selectorElement.style.top = ty +"px";
-            selectorElement.style.display = 'block';
-        }
-    }
-
     function prepareField() {
+        const quadrantElements = [];
         for (let q = 0; q < 9; q++) {
             const quadrant = document.createElement('div');
             quadrant.classList.add('quadrant', 'grid3x3');
@@ -70,15 +25,14 @@ document.addEventListener('DOMContentLoaded', function(){
             quadrantElements[Math.floor(i / 27) * 3 + Math.floor((i / 3) % 3)].append(cellElement);
         }
     }
-
     const fieldElement = document.getElementById('sudoku-grid');
     const selectorElement = document.getElementById('numberSelector');
+    const {render : renderField, clear: clearField } = getFieldUI(fieldElement);
+    const {show: showSelector, hide: hideSelector} = getSelectorUI(selectorElement);
     const winConditionModalElement = document.getElementById('winConditionModal');
-    const eventEmitter = new EventEmitter();
-    let field;
-    let selector;
-    const cellElements = [];
-    const quadrantElements = [];
+    const emitter = new EventEmitter();
+    let [cellSelected, supposedFlag ] = [-1, false];
+    emitter.subscribe(events.GAME_START, startGameSubscriber(emitter))
 
     prepareField();    
 
@@ -94,64 +48,56 @@ document.addEventListener('DOMContentLoaded', function(){
     })
 
     document.getElementById('startNewGameButton').addEventListener('click', function(event){
+        function renderFieldSubscriber({payload: {field}}){
+            console.trace('ValueSetSubscriber subscriber triggered');
+            renderField(field);
+        }
+
+        function winConditionModalSubscriber(){
+            console.trace('winConditionModalSubscriber triggered');
+            winConditionModalElement.style.display = 'block';
+        }
+
         const startCellsNum = document.getElementById('difficultyRange').value;
-        field = generator.createFieldOfNumberOfCells(startCellsNum);
-        selector = Selector.forField(field);
-        eventEmitter.emit({ 
-            event: events.GAME_START, 
-            startCellsNum,
-            field
+        emitter.subscribe(events.FIELD_UPDATED, renderFieldSubscriber);
+        emitter.subscribe(events.WIN_CONDITION, winConditionModalSubscriber);
+        emitter.subscribe(events.WIN_CONDITION, function dispose(){
+            emitter.unSubscribe(events.FIELD_UPDATED, renderFieldSubscriber);
+            emitter.unSubscribe(events.WIN_CONDITION, winConditionModalSubscriber);
+            emitter.unSubscribe(events.WIN_CONDITION, dispose);
         });
-        eventEmitter.subscribe(events.VALUE_SET, winCheckSubscriber(field, eventEmitter));
-        eventEmitter.subscribe(events.WIN_CONDITION, winConditionSubscriber(field, selector, eventEmitter));
-        eventEmitter.subscribe(events.WIN_CONDITION, function(){
-            document.getElementById('winConditionModal').style.display = "block";
-        })
-        renderField(field);        
+        emitter.emit({eventName: events.GAME_START, payload: {startCellsNum}});
     });
     
     document.getElementById('restartGameButton').addEventListener('click', function(event){
-        eventEmitter.emit({ 
-            event: events.GAME_RESTART
-        });
+        emitter.emit({ eventName: events.GAME_RESTART, payload: {startCellsNum } });
     });
 
     winConditionModalElement
-    .querySelector('.fa-window-close')
-        .addEventListener('click', function (){
-            winConditionModalElement.style.display = 'none';
-            
-    });
+        .querySelector('.fa-window-close')
+            .addEventListener('click', function (){
+                winConditionModalElement.style.display = 'none';
+        });
 
     selectorElement
         .querySelector('.fa-window-close')
         .addEventListener('click', function (){
-            selector.clearSelection();
-            selectorElement.style.display = 'none';
-            renderField(field);
+            hideSelector();
     });
     
     fieldElement.querySelectorAll('.cell')
         .forEach(e => e.addEventListener('click', function (event) {
-            selector.select(e.dataset.index, Boolean(event.ctrlKey));
-            eventEmitter.emit({ 
-                event: events.CELL_SELECTED, 
-                value: e.dataset.index 
-            });
-            renderSelector(selector, event.x, event.y);
-            renderField(field);
+            [cellSelected, supposedFlag] = [e.dataset.index, Boolean(event.ctrlKey)];
+            showSelector({lx: event.x, ty: event.y});
     }));
 
     selectorElement.querySelectorAll('.cell').forEach((e, i) => {
         e.addEventListener('click', function (event) {
-            selector.setValue(i + 1);
-            eventEmitter.emit({ 
-                event: events.VALUE_SET, 
-                value: i + 1
+            emitter.emit({ 
+                eventName: events.VALUE_SET, 
+                payload: { value: i + 1, index: cellSelected, suppossed: supposedFlag }    
             });
-            renderSelector(selector);
-            renderField(field);
-        })
+        });
     });
 
     document.getElementById('gameForm').addEventListener('submit', function(event){
--- src/main/game/gameLifeCycle.js
+++ src/main/game/gameLifeCycle.js
@@ -1,10 +1,12 @@
 import { hasWinCondition, allCellsFilled } from "./fieldMonitoring.js";
+import Generator from '../generator/generator.js'
 
 const events = Object.freeze({
     GAME_START: 'gameStart',
     GAME_RESTART: 'gameRestart',
     VALUE_SET: 'valueSet',
     CELL_SELECTED: 'cellSelected',
+    FIELD_UPDATED: 'fieldUpdated',
     FIELD_FILLED: 'fieldIsFull',
     WIN_CONDITION: 'winCondition'
 });
@@ -13,10 +15,10 @@ class EventEmitter{
     constructor(){
         this.listeners = new Map();
     }
-    emit(data){
-        if(this.listeners.has(data.event)){
-            for(const fn of this.listeners.get(data.event) ){
-                fn(data);
+    emit(event){
+        if(this.listeners.has(event.eventName)){
+            for(const fn of this.listeners.get(event.eventName) ){
+                fn(event);
             }
         }
     }
@@ -36,19 +38,47 @@ class EventEmitter{
     }
 }
 
-function winCheckSubscriber(field, emitter){
-    return function(event){
+// function cellSelectionSubscriber(emitter, selector){
+//     return function({payload: {index, supposed}}){
+//         selector.select(index, supposed);
+//     }
+// }
+
+function valueSetSubscriber(emitter, field){
+    return function({payload: {index, value, supposed}}){
+        console.trace('ValueSetSubscriber subscriber triggered');
+        field.setValue(index, value, supposed);
+        emitter.emit({eventName: events.FIELD_UPDATED, payload: {field}});
+    }
+}
+
+function winCheckSubscriber(emitter, field){
+    return function(){
+        console.trace('WinCheckSubscriber subscriber triggered');
         if(allCellsFilled(field.getValuesFlat()) && hasWinCondition(field.getValuesFlat())){
-            emitter.emit({event: events.WIN_CONDITION})
+            emitter.emit({eventName: events.WIN_CONDITION})
         }
     }
 }
 
-function winConditionSubscriber(field, selector, emitter){
-    
-    return function(event){
-        selector.disable();
+function startGameSubscriber(emitter){
+    return function({payload: {startCellsNum}}){
+        console.trace('StartGameSubscriber triggered');
+        const generator = new Generator();
+        const field = generator.createFieldOfNumberOfCells(startCellsNum);
+
+        const winCheckSubscriberFn = winCheckSubscriber(emitter, field);
+        const valueSetSubscriberFn = valueSetSubscriber(emitter, field);
+
+        emitter.subscribe(events.VALUE_SET, valueSetSubscriberFn);
+        emitter.subscribe(events.VALUE_SET, winCheckSubscriberFn);
+        emitter.subscribe(events.WIN_CONDITION, function dispose(){
+            emitter.unSubscribe(events.VALUE_SET, winCheckSubscriberFn);
+            emitter.unSubscribe(events.VALUE_SET, valueSetSubscriberFn);
+            emitter.unSubscribe(events.WIN_CONDITION, dispose);
+        });
+        emitter.emit({eventName: events.FIELD_UPDATED, payload: {field}})
     }
 }
 
-export {EventEmitter, events, winCheckSubscriber, winConditionSubscriber};
+export {EventEmitter, events, startGameSubscriber};
