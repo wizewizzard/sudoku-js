116f4a8a51371e7dbf12722960e405b368bc5c35
wizewizzard <kingaal93@gmail.com>
2022-12-01 21:44:09 +0700

    Add GAME_ENDED event, extracted render field and selector functions

--- src/main/game/gameLifeCycle.js
+++ src/main/game/gameLifeCycle.js
@@ -4,7 +4,9 @@ import Generator from '../generator/generator.js'
 const events = Object.freeze({
     GAME_START: 'gameStart',
     GAME_RESTART: 'gameRestart',
+    GAME_ENDED: 'gameEnded',
     VALUE_SET: 'valueSet',
+    CELL_SELECT: 'cellSelect',
     CELL_SELECTED: 'cellSelected',
     FIELD_UPDATED: 'fieldUpdated',
     FIELD_FILLED: 'fieldIsFull',
@@ -31,53 +33,56 @@ class EventEmitter{
     unSubscribe(eventName, fn){
         if(this.listeners.has(eventName)){
             const fns = this.listeners.get(eventName);
-            const index = fns.indexOf(fn);
-            if(index >= 0)
-                fns.splice(index, 1);
-        }
-    }
-}
-
-// function cellSelectionSubscriber(emitter, selector){
-//     return function({payload: {index, supposed}}){
-//         selector.select(index, supposed);
-//     }
-// }
-
-function valueSetSubscriber(emitter, field){
-    return function({payload: {index, value, supposed}}){
-        console.trace('ValueSetSubscriber subscriber triggered');
-        field.setValue(index, value, supposed);
-        emitter.emit({eventName: events.FIELD_UPDATED, payload: {field}});
-    }
-}
-
-function winCheckSubscriber(emitter, field){
-    return function(){
-        console.trace('WinCheckSubscriber subscriber triggered');
-        if(allCellsFilled(field.getValuesFlat()) && hasWinCondition(field.getValuesFlat())){
-            emitter.emit({eventName: events.WIN_CONDITION})
+            fns.delete(fn);
         }
     }
 }
 
 function startGameSubscriber(emitter){
-    return function({payload: {startCellsNum}}){
-        console.trace('StartGameSubscriber triggered');
+    function valueSetSubscriber(emitter, field){
+        return function({data: {index, value, supposed, ...rest}}){
+            //console.trace('ValueSetSubscriber subscriber triggered');
+            field.setValue(index, value, supposed);
+            emitter.emit({eventName: events.FIELD_UPDATED, data: {field}});
+        }
+    }
+    
+    function winCheckSubscriber(emitter, field){
+        return function(){
+            //console.trace('WinCheckSubscriber subscriber triggered');
+            if(allCellsFilled(field.getValuesFlat()) && hasWinCondition(field.getValuesFlat())){
+                emitter.emit({eventName: events.WIN_CONDITION});
+                emitter.emit({eventName: events.GAME_ENDED});
+            }
+        }
+    }
+
+    function cellSelectSubscriber(emitter, field){
+        return function({data: {index, ...rest}}){
+            //console.trace('cellSelectSubscriber triggered');
+            emitter.emit({eventName: events.CELL_SELECTED, data: {cell: field.getCell(index), ...rest}})
+        }
+    }
+
+    return function({data: {startCellsNum, ...rest}}){
+        //console.trace('StartGameSubscriber triggered');
         const generator = new Generator();
         const field = generator.createFieldOfNumberOfCells(startCellsNum);
 
         const winCheckSubscriberFn = winCheckSubscriber(emitter, field);
         const valueSetSubscriberFn = valueSetSubscriber(emitter, field);
+        const cellSelectSubscriberFn =  cellSelectSubscriber(emitter, field);
 
         emitter.subscribe(events.VALUE_SET, valueSetSubscriberFn);
         emitter.subscribe(events.VALUE_SET, winCheckSubscriberFn);
-        emitter.subscribe(events.WIN_CONDITION, function dispose(){
+        emitter.subscribe(events.CELL_SELECT, cellSelectSubscriberFn);
+        emitter.subscribe(events.GAME_ENDED, function dispose(){
             emitter.unSubscribe(events.VALUE_SET, winCheckSubscriberFn);
             emitter.unSubscribe(events.VALUE_SET, valueSetSubscriberFn);
+            emitter.unSubscribe(events.CELL_SELECT, cellSelectSubscriberFn);
             emitter.unSubscribe(events.WIN_CONDITION, dispose);
         });
-        emitter.emit({eventName: events.FIELD_UPDATED, payload: {field}})
+        emitter.emit({eventName: events.FIELD_UPDATED, data: {field}})
     }
 }
 
